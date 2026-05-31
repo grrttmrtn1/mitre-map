44539cb4665f98dd2fd6f3be9f5a0737e3f7f9b5
Garrett Martin <grrttmrtn2@gmail.com>
2026-05-30 20:15:18 -0500

    feat: add compliance_snapshots table

---
+++ server/src/db/migrations/017_compliance_snapshots.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTableIfNotExists('compliance_snapshots', t => {
+    t.increments('id').primary();
+    t.string('framework_id').notNullable();
+    t.integer('total_controls').notNullable();
+    t.integer('covered_controls').notNullable();
+    t.integer('coverage_pct').notNullable();
+    t.timestamp('taken_at').defaultTo(knex.fn.now());
+  });
+  await knex.schema.raw(
+    'CREATE INDEX IF NOT EXISTS idx_compliance_snapshots_fw_time ON compliance_snapshots(framework_id, taken_at DESC)'
+  );
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('compliance_snapshots');
+}
